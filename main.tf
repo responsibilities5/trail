8ee326ab2bb8d128659a7b27c10191f3e470852b
responsibilities5 <45757209+responsibilities5@users.noreply.github.com>
2021-04-29 16:47:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,10 +24,22 @@ resource "aws_instance" "ec2" {
     network_interface_id = aws_network_interface.interface.id
   }
   
-  #user_data = <<-EOF
-  #            #!/bin/sh
-   #           sudo yum 
-  #
+  user_data = <<-EOF
+              #!/bin/sh
+              sudo apt update
+              sudo apt-get update
+              sudo apt-get remove docker docker-engine docker.io containerd runc
+              sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+              echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+              sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+              sudo usermod -aG docker ubuntu
+              exit
+              
+              EOF
+              
+  
   
   tags = {
     Name = "ubuntu"
