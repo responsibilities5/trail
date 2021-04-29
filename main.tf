1512147d66e971d0a78cf825995f951de5f641f8
responsibilities5 <45757209+responsibilities5@users.noreply.github.com>
2021-04-29 06:06:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,6 +91,14 @@ resource "aws_security_group" "security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
