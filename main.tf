67bd1dc91f93e001777bbf2e95e558533cf3e774
responsibilities5 <45757209+responsibilities5@users.noreply.github.com>
2021-05-01 13:21:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,6 @@ resource "aws_instance" "ec2" {
               sudo aws configure set aws_secret_access_key ${var.aws_secret_key}
               sudo aws configure set default.region us-east-1
               sudo aws configure set default.output json
-              sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 308682804621.dkr.ecr.us-east-1.amazonaws.com
               exit
               
               EOF
